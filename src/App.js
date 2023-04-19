6ae26244e1b259ae440a092199cd6b6275d696a0
Brian Janes <janes.brian@gmail.com>
2023-04-18 22:54:51 -0230

    global styled added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,37 @@
 import React from "react";
+import GolbalStyles from "./GolbalStyles";
+import styled from "styled-components";
 
 const App = () => {
-  return <div>Kieran is smart</div>;
+  return (
+    <BackgroundColor>
+      <Container>
+        <GolbalStyles />
+        <Main>Test</Main>
+      </Container>
+    </BackgroundColor>
+  );
 };
+
+const BackgroundColor = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100vw;
+  height: 100vh;
+  background-color: lightgray;
+`;
+const Container = styled.div`
+  width: min(100% - 2rem, 800px);
+  margin-inline: auto;
+  height: 80vh;
+  border-radius: 2rem;
+  box-shadow: 2px 2px 4px 2px rgba(0, 0, 0, 0.5);
+  background-color: whitesmoke;
+`;
+
+const Main = styled.div`
+  padding: 2rem;
+`;
 
 export default App;
